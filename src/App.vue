3cf5d656c18b900d9b71a994ff1a828f39d2e6dc
silviosalza <silviofilipposalza@gmail.com>
2023-06-13 19:09:13 +0200

    milestone 4-5

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <script>
+import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 export default{
+  components: {
+    ProjectCard,
+  },
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  mounted(){
+    this.getProjects();
+  },
+  methods: {
+    getProjects(){
+      axios.get('http://127.0.0.1:8000/api/projects').then(resp => {
+        console.log(resp);
+        this.projects = resp.data.result;
+      })
+
+
+    }
+  },
 
 }
 </script>
 
 <template>
-  <h1>Ciao</h1>
+  
+  <div class="container">
+    <h1 class="text-center">BoolFolio</h1>
+    <div class="row row-cols-4">
+      <div class="col" v-for="(element,index) in projects" :key="index" >
+        <ProjectCard :project="element" />
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    name: "ProjectCard",
+    props:{
+        project: Object,
+    }
+}
+</script>
+
+<template>
+        <div class="card text-center">
+            <h4>{{ project.title }}</h4>
+            <p>{{ project.content }}</p>
+        </div>
+</template>
+
+
+
+<style scoped lang="scss">
+.card{
+    height: 500px;
+
+}
+
+</style>
