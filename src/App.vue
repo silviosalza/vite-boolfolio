1de12021e7652569a84ddb2a24bdd8cc1b863d37
silviosalza <silviofilipposalza@gmail.com>
2023-06-15 17:48:03 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <script>
 import AppAbout from './components/AppAbout.vue';
+import AppHeader from './components/AppHeader.vue';
 import HomePage from './pages/HomePage.vue';
 import ProjectsList from '../src/pages/ProjectsList.vue';
 
 
 export default{
-    components: { HomePage, AppAbout, ProjectsList }
+    components: { HomePage, AppAbout, ProjectsList, AppHeader }
 }
 
 </script>
 
 <template>
+  <AppHeader/>
   <!-- <HomePage/> -->
   <!-- <ProjectsList/> -->
   <router-view></router-view>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,45 @@
+<script>
+export default{
+
+    name: 'AppHeader',
+
+    data() {
+        return {
+            menuItems: [
+                {
+                    label: "Home",
+                    routeName: "home"
+                },
+                {
+                    label: "Projects",
+                    routeName: "projects"
+                },
+               
+            ]
+        }
+}
+}
+
+</script>
+
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">BoolFolio</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item" v-for="item in menuItems">
+                            <router-link :to="{ name: item.routeName }" class="nav-link active" aria-current="page">{{
+                                item.label }}</router-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
